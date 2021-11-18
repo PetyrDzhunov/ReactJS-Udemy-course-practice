1eac3af4813cf90006431ef4078c42b6a393bf33
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2021-11-18 22:32:34 +0200

    useEffect function to make our button use the bump

--- food-order-app/src/components/Layout/HeaderCartButton.js
+++ food-order-app/src/components/Layout/HeaderCartButton.js
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import CartIcon from '../Cart/CartIcon';
 import CartContext from '../../store/cart-context';
 
@@ -6,12 +6,36 @@ import classes from './HeaderCartButton.module.css';
 
 
 const HeaderCartButton = (props) => {
+	const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
+
 	const cartCtx = useContext(CartContext)
-	const numberOfCartItems = cartCtx.items.reduce((currNumber, item) => {
+	const { items } = cartCtx
+	const numberOfCartItems = items.reduce((currNumber, item) => {
 		return currNumber + item.amount;
 	}, 0);
+
+	const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump : ''}`;
+	//change button classes to include the bump and then we set
+	// a timer which removes that class so that when it's added 
+	// again in the future it plays again;
+	useEffect(() => {
+		if (items.length === 0) {
+			return;
+		};
+
+		setBtnIsHighlighted(true);
+
+		const timer = setTimeout(() => {
+			setBtnIsHighlighted(false);
+		}, 300)
+
+		return () => {
+			clearTimeout(timer);
+		};
+	}, [items]);
+
 	return (
-		<button onClick={props.onClick} className={classes.button}>
+		<button onClick={props.onClick} className={btnClasses}>
 			<span className={classes.icon}>
 				<CartIcon />
 			</span>
