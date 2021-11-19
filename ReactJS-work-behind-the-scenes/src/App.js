d48226c3194d8327278d329ef26204ff8dce52ae
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2021-11-19 11:00:26 +0200

    learn react.memo optimization and useCallback to work with ref data types on props

--- ReactJS-work-behind-the-scenes/src/App.js
+++ ReactJS-work-behind-the-scenes/src/App.js
@@ -1,20 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 
+import DemoOutput from './components/Demo/DemoOutput';
 import Button from './components/UI/Button/Button';
 import './App.css';
 
 function App() {
+  console.log('APP RUNNING');
 
   const [showParagraph, setShowParagraph] = useState(false);
 
-  const toggleParagraphHandler = () => {
-    setShowParagraph((prevShowParagraph) => !prevShowParagraph)
-  };
+  const toggleParagraphHandler = useCallback(() => {
+    setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+  }, []);
 
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      {showParagraph && <p>This is New!</p>}
+      <DemoOutput show={false} />
       <Button onClick={toggleParagraphHandler}>Toogle Paragraph!</Button>
     </div>
   );
