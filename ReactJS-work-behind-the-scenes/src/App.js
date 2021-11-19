8bc28f88e122e285f875d253a640c613357a9739
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2021-11-19 12:51:38 +0200

    learning the class-based-components

--- ReactJS-work-behind-the-scenes/src/App.js
+++ ReactJS-work-behind-the-scenes/src/App.js
@@ -5,18 +5,27 @@ import Button from './components/UI/Button/Button';
 import './App.css';
 
 function App() {
-  console.log('APP RUNNING');
 
   const [showParagraph, setShowParagraph] = useState(false);
+  const [allowToggle, setAllowToggle] = useState(false);
+
+  console.log('APP RUNNING');
 
   const toggleParagraphHandler = useCallback(() => {
-    setShowParagraph((prevShowParagraph) => !prevShowParagraph);
-  }, []);
+    if (allowToggle) {
+      setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+    };
+  }, [allowToggle]);
+
+  const allowToggleHandler = () => {
+    setAllowToggle(true);
+  };
 
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      <DemoOutput show={false} />
+      <DemoOutput show={showParagraph} />
+      <Button onClick={allowToggleHandler}>Allow Toggle</Button>
       <Button onClick={toggleParagraphHandler}>Toogle Paragraph!</Button>
     </div>
   );
