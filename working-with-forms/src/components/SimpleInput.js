55263dfe252f96f146ceaece08a29bc1ab7af080
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2021-11-20 15:27:54 +0200

    add one more input field to validate and make good UX with it , yet this code looks way too much -> useReducer maybe

--- working-with-forms/src/components/SimpleInput.js
+++ working-with-forms/src/components/SimpleInput.js
@@ -1,13 +1,35 @@
 import { useState, useEffect } from "react";
 
+// add second input where we fetch the email of the user
+// get the value and validate it if it is a valid email adress
+
 const SimpleInput = (props) => {
 
   const [enteredName, setEnteredName] = useState('');
   const [enteredNameTouched, setEnteredNameTouched] = useState(false);
 
-  const enteredNameIsValid = enteredName.trim() !== '';
-  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched
+  const [enteredEmail, setEnteredEmail] = useState('');
+  const [enteredEmailTouched, setEnteredEmailTouched] = useState(false);
 
+  const enteredNameIsValid = enteredName.trim() !== '';
+  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
+
+  const enteredEmailIsValid = enteredEmail.trim() !== '' && enteredEmail.includes('@'); // true
+  const emailInputIsValid = !enteredEmailIsValid && enteredEmailTouched;
+
+  let formIsValid = false;
+
+  if (enteredNameIsValid && enteredEmailIsValid) {
+    formIsValid = true;
+  };
+
+  const emailInputChangeHandler = (e) => {
+    setEnteredEmail(e.target.value);
+  };
+
+  const emailInputBlurHandler = (e) => {
+    setEnteredEmailTouched(true);
+  };
 
   const nameInputChangeHandler = (e) => {
     setEnteredName(e.target.value);
@@ -25,9 +47,13 @@ const SimpleInput = (props) => {
     };
     setEnteredName('');
     setEnteredNameTouched(false);
+
+    setEnteredEmail('');
+    setEnteredEmailTouched(false);
   };
 
   const nameInputClasses = nameInputIsInvalid ? 'form-control invalid' : 'form-control';
+  const emailInputClasses = emailInputIsValid ? 'form-control invalid' : 'form-control';
 
   return (
     <form onSubmit={formSubmissionHandler}>
@@ -42,8 +68,20 @@ const SimpleInput = (props) => {
         />
         {nameInputIsInvalid && <p className="error-text">Name must not be empty</p>}
       </div>
+
+      <div className={emailInputClasses}>
+        <label htmlFor='email'>Your E-mail</label>
+        <input
+          onChange={emailInputChangeHandler}
+          onBlur={emailInputBlurHandler}
+          type='email'
+          id='email'
+          value={enteredEmail}
+        />
+        {emailInputIsValid && <p className="error-text">Please enter a valid email</p>}
+      </div>
       <div className="form-actions">
-        <button>Submit</button>
+        <button disabled={!formIsValid}>Submit</button>
       </div>
     </form>
   );
