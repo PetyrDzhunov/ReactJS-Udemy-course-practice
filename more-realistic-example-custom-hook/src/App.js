6e3c335b616a05045306caf5991996bfd93b309d
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2021-11-20 12:26:15 +0200

    finished our hook now we will use it whereever we want in our app

--- more-realistic-example-custom-hook/src/App.js
+++ more-realistic-example-custom-hook/src/App.js
@@ -1,13 +1,31 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 
 import Tasks from './components/Tasks/Tasks';
 import NewTask from './components/NewTask/NewTask';
+import useHttp from './components/hooks/use-http';
+
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+
+
+
+  const httpData = useHttp();
+
+  const { isLoading, error, sendRequest: fetchTasks } = httpData;
 
 
   useEffect(() => {
-    fetchTasks();
+    const transformTasks = (tasksObject) => {
+      const loadedTasks = [];
+      for (const taskKey in tasksObject) {
+        loadedTasks.push({ id: taskKey, text: tasksObject[taskKey].text });
+      };
+      setTasks(loadedTasks);
+    };
+    fetchTasks({
+      url: 'https://react-demo-d50c0-default-rtdb.europe-west1.firebasedatabase.app/tasks.json',
+    }, transformTasks);
   }, []);
 
   const taskAddHandler = (task) => {
--- more-realistic-example-custom-hook/src/components/hooks/use-http.js
+++ more-realistic-example-custom-hook/src/components/hooks/use-http.js
@@ -1,19 +1,19 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 
 
 const useHttp = () => {
 	const [isLoading, setIsLoading] = useState(false);
 	const [error, setError] = useState(null);
 
-	const sendRequest = async (requestConfig, applyData) => {
+	const sendRequest = useCallback(async (requestConfig, applyData) => {
 		setIsLoading(true);
 		setError(null);
 		try {
 			const response = await fetch(
 				requestConfig.url, {
-				method: requestConfig.method,
-				headers: requestConfig.headers,
-				body: JSON.stringify(requestConfig.body)
+				method: requestConfig.method ? requestConfig.method : "GET",
+				headers: requestConfig.headers ? requestConfig.headers : {},
+				body: requestConfig.body ? JSON.stringify(requestConfig.body) : null
 			});
 
 
@@ -24,12 +24,11 @@ const useHttp = () => {
 			const data = await response.json();
 			applyData(data);
 
-			setTasks(loadedTasks);
 		} catch (err) {
 			setError(err.message || 'Something went wrong!');
 		};
 		setIsLoading(false);
-	};
+	}, []);
 
 	return {
 		isLoading,
