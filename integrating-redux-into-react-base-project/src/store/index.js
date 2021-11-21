b069fed534df79b20a5f3d5471a34b8df1be7ad9
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2021-11-21 16:11:35 +0200

    control more than 1 state with redux and understand why we should never mutate the original state, instead we should always return brand new state object

--- integrating-redux-into-react-base-project/src/store/index.js
+++ integrating-redux-into-react-base-project/src/store/index.js
@@ -1,13 +1,26 @@
 import { createStore } from 'redux';
 
-const counterReducer = (state = { counter: 0 }, action) => {
+const initialState = { counter: 0, showCounter: true };
+
+const counterReducer = (state = initialState, action) => {
 	let { type } = action;
 	if (type === 'increment') {
-		return { counter: state.counter + 1 };
+		return { counter: state.counter + 1, showCounter: state.showCounter };
 	};
 
 	if (type === 'decrement') {
-		return { counter: state.counter - 1 };
+		return { counter: state.counter - 1, showCounter: state.showCounter };
+	};
+
+	if (type === 'increase') {
+		return { counter: state.counter + action.amount, showCounter: state.showCounter };
+	};
+
+	if (type === 'toggle') {
+		return {
+			showCounter: !state.showCounter,
+			counter: state.counter
+		};
 	};
 
 	return state;
