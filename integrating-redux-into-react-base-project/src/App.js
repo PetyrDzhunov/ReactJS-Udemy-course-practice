65037eb2efda57860b11e2c1f9d5537bc125f02b
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2021-11-21 17:37:47 +0200

    made some conditional rendering with data we get from the one of the two redux slices we created with redux toolkit , so now we work with multiple slices

--- integrating-redux-into-react-base-project/src/App.js
+++ integrating-redux-into-react-base-project/src/App.js
@@ -1,12 +1,21 @@
 import Counter from './components/Counter';
 import Header from './components/Header';
-
+import Auth from './components/Auth';
+import { authActions } from './store/index'
+import store from './store/index';
+import { useSelector } from 'react-redux';
+import UserProfile from './components/UserProfile';
 
 function App() {
+  const isAuthenticated = useSelector(state => state.auth.isAuthenticated);
+
+
   return (
     <>
-      <Counter />
       <Header />
+      {!isAuthenticated && <Auth />}
+      {isAuthenticated && <UserProfile />}
+      <Counter />
     </>
   );
 }
