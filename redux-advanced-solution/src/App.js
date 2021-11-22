ab80c45767d8970be7d07a580ed70922d0062dcc
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2021-11-22 13:57:54 +0200

    understand what's the problem with useEffect when handling the http request side-effect and fixed it

--- redux-advanced-solution/src/App.js
+++ redux-advanced-solution/src/App.js
@@ -4,24 +4,71 @@ import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 
+import { uiActions } from './store/ui-slice';
+import { useDispatch } from 'react-redux';
+import Notification from './components/UI/Notification';
+
 
 
 function App() {
   const { cartIsVisible } = useSelector(state => state.ui);
   const cart = useSelector(state => state.cart);
+  const dispatch = useDispatch();
+  const notification = useSelector(state => state.ui.notification);
+
+  let isInitial = true;
 
   useEffect(() => {
-    fetch('https://react-demo-d50c0-default-rtdb.europe-west1.firebasedatabase.app/cart.json', {
-      method: 'PUT',
-      body: JSON.stringify(cart)
-    })
-  }, [cart]);
+    const sendCartData = async () => {
+      dispatch(uiActions.showNotification({
+        status: 'pending',
+        title: "Sending...",
+        message: "Sending cart data!",
+      }));
+      const response = await fetch('https://react-demo-d50c0-default-rtdb.europe-west1.firebasedatabase.app/cart.json', {
+        method: 'PUT',
+        body: JSON.stringify(cart)
+      });
+
+      if (!response.ok) {
+        throw new Error('Sending cart data failed.')
+      };
+
+      dispatch(uiActions.showNotification({
+        status: 'success',
+        title: "Success!",
+        message: "Sent cart data successfully",
+      }));
+
+    };
+    if (isInitial) {
+      isInitial = false;
+      return
+    };
+
+    sendCartData().catch(error => {
+      dispatch(uiActions.showNotification({
+        status: "error",
+        title: "Error!",
+        message: "Sending cart data failed!"
+      }));
+    });
+
+  }, [cart, dispatch]);
 
   return (
-    <Layout>
-      {cartIsVisible && <Cart />}
-      <Products />
-    </Layout>
+    <>
+      {notification &&
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />}
+      <Layout>
+        {cartIsVisible && <Cart />}
+        <Products />
+      </Layout>
+    </>
   );
 }
 
