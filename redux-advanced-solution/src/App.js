0d28d5d52408f450378af88d411e108db160e3bb
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2021-11-22 16:12:41 +0200

    implemented HTTP and side effect in redux and learned different approaches of doing that

--- redux-advanced-solution/src/App.js
+++ redux-advanced-solution/src/App.js
@@ -16,6 +16,8 @@ function App() {
   const dispatch = useDispatch();
   const notification = useSelector(state => state.ui.notification);
 
+
+
   useEffect(() => {
     dispatch(fetchCartData());
   }, [dispatch]);
@@ -23,12 +25,12 @@ function App() {
 
   useEffect(() => {
     if (isInitial) {
-
       isInitial = false;
       return;
     };
-
-    dispatch(sendCartData(cart));
+    if (cart.changed) {
+      dispatch(sendCartData(cart));
+    };
   }, [cart, dispatch]);
 
   return (
