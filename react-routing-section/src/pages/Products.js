d062c6b3fa58332500dcac8df8bbc068ac440446
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2021-11-23 22:16:10 +0200

    learn the Switch component use and the exact 'prop

--- react-routing-section/src/pages/Products.js
+++ react-routing-section/src/pages/Products.js
@@ -1,12 +1,15 @@
+import { Link } from "react-router-dom";
+
+
 
 const Products = () => {
 	return (
 		<section>
 			<h1>The Products Page</h1>
 			<ul>
-				<li>Product 1</li>
-				<li>Product 2</li>
-				<li>Product 3</li>
+				<Link to='/products/p1'>Product 1</Link>
+				<Link to='/products/p2'>Product 2</Link>
+				<Link to='/products/p3'>Product 3</Link>
 			</ul>
 		</section>
 	);
