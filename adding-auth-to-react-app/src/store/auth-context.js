f323f8a2f2e2e564cca8e96bf99e631c54a46ed9
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2021-11-25 00:47:40 +0200

    persisting the  user authentication status with localStorage

--- adding-auth-to-react-app/src/store/auth-context.js
+++ adding-auth-to-react-app/src/store/auth-context.js
@@ -9,15 +9,18 @@ const AuthContext = React.createContext({
 });
 
 export const AuthContextProvider = (props) => {
-	const [token, setToken] = useState(null);
+	const initialToken = localStorage.getItem('token');
+	const [token, setToken] = useState(initialToken);
 	const userIsLoggedIn = !!token; // converts truthy/falsy value to true/false boolean value;
 
 	const loginHandler = (token) => {
 		setToken(token);
+		localStorage.setItem('token', token);
 	};
 
 	const logoutHandler = () => {
 		setToken(null)
+		localStorage.removeItem('token');
 	};
 
 	const contextValue = {
