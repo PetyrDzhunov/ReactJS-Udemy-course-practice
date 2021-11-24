ba730127f6aa1b6c9d8fa4ae2c79b786e8ab14c3
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2021-11-24 23:33:26 +0200

    create user with sending to the firebase API a post with our data

--- adding-auth-to-react-app/src/components/Auth/AuthForm.js
+++ adding-auth-to-react-app/src/components/Auth/AuthForm.js
@@ -1,25 +1,62 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 
 import classes from './AuthForm.module.css';
+const API_KEY = 'AIzaSyB6CQIjIKARZ5p4w3MtYJeWh5PpSZJDTLg';
 
 const AuthForm = () => {
+  let emailInputRef = useRef();
+  let passwordInputRef = useRef();
   const [isLogin, setIsLogin] = useState(true);
 
   const switchAuthModeHandler = () => {
     setIsLogin((prevState) => !prevState);
   };
 
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const enteredEmail = emailInputRef.current.value;
+    const enteredPassword = passwordInputRef.current.value;
+
+    //optional : Add validaton
+    if (isLogin) {
+
+    } else {
+      const body = {
+        email: enteredEmail,
+        password: enteredPassword,
+        returnSecureToken: true
+      };
+      fetch(`https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(body)
+      }).then(res => {
+        if (res.ok) {
+
+        } else {
+          return res.json().then(data => {
+            //show error modal
+            console.log(data);
+          });
+        }
+      }).catch(err => console.log(err));
+    };
+
+  };
+
   return (
     <section className={classes.auth}>
       <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
-      <form>
+      <form onSubmit={submitHandler}>
         <div className={classes.control}>
           <label htmlFor='email'>Your Email</label>
-          <input type='email' id='email' required />
+          <input ref={emailInputRef} type='email' id='email' required />
         </div>
         <div className={classes.control}>
           <label htmlFor='password'>Your Password</label>
-          <input type='password' id='password' required />
+          <input ref={passwordInputRef} type='password' id='password' required />
         </div>
         <div className={classes.actions}>
           <button>{isLogin ? 'Login' : 'Create Account'}</button>
