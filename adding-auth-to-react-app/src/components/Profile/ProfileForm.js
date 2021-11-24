6680e2bc0abf7027a3e62429cb9e6429707de2e8
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2021-11-25 00:24:38 +0200

    created change-password functionality with firebase

--- adding-auth-to-react-app/src/components/Profile/ProfileForm.js
+++ adding-auth-to-react-app/src/components/Profile/ProfileForm.js
@@ -1,11 +1,38 @@
 import classes from './ProfileForm.module.css';
+import { useRef, useContext } from 'react';
+import AuthContext from '../../store/auth-context';
+
+const API_KEY = 'AIzaSyB6CQIjIKARZ5p4w3MtYJeWh5PpSZJDTLg';
+
 
 const ProfileForm = () => {
+  const authCtx = useContext(AuthContext);
+  const newPasswordInputRef = useRef();
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const enteredNewPassword = newPasswordInputRef.current.value;
+    //add validation;
+    fetch(`https://identitytoolkit.googleapis.com/v1/accounts:update?key=${API_KEY}`, {
+      method: 'POST',
+      body: JSON.stringify({
+        idToken: authCtx.token,
+        password: enteredNewPassword,
+        returnSecureToken: false
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      },
+    }).then(res => {
+
+    })
+  };
+
   return (
-    <form className={classes.form}>
+    <form onSubmit={submitHandler} className={classes.form}>
       <div className={classes.control}>
         <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
+        <input type='password' id='new-password' minLength="7" ref={newPasswordInputRef} />
       </div>
       <div className={classes.action}>
         <button>Change Password</button>
