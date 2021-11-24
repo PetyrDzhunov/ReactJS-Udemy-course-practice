460140a5be34ad35bc3eb1bce59dcc0f9f4a1271
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2021-11-24 17:27:04 +0200

    better way to create nested routes and use them in react-router version 6

--- react-routing-section/src/pages/Welcome.js
+++ react-routing-section/src/pages/Welcome.js
@@ -1,16 +1,11 @@
-import { Route, Routes } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Welcome = () => {
 	return (
 		<section>
 			<h1>The Welcome Page</h1>
-			<Routes>
-				<Route path='/welcome/new-user'>
-
-				</Route>
-			</Routes>
-
-		</section>
+			<Link to='new-user'>New User</Link>
+		</section >
 	);
 
 };
