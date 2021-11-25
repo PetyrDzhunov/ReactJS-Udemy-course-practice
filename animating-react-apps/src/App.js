fe00201dc21d1d7af1c95651932c6951f9c69145
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2021-11-25 12:36:18 +0200

    react-transition-group -> a third party library to help us with our animatiomations

--- animating-react-apps/src/App.js
+++ animating-react-apps/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Transition from 'react-transition-group/Transition';
 
 import "./App.css";
 import Modal from "./components/Modal/Modal";
@@ -7,7 +8,8 @@ import List from "./components/List/List";
 
 class App extends Component {
   state = {
-    modalIsOpen: false
+    modalIsOpen: false,
+    showBlock: false
   };
 
   showModal = () => {
@@ -22,12 +24,32 @@ class App extends Component {
     return (
       <div className="App">
         <h1>React Animations</h1>
+        <button className="Button" onClick={() => this.setState(prevState => ({ showBlock: !prevState.showBlock }))}>Toggle</button>
+        <br />
+        <br />
+        <br />
+        <Transition in={this.state.showBlock} timeout={300} >
+          {state => (
+            <div
+              style={{
+                margin: 'auto',
+                backgroundColor: 'red',
+                width: 100,
+                height: 100,
+                transition: 'opacity 1s ease-out',
+                opacity: state === 'exited' ? 0 : 1
+
+              }}
+            />
+          )}
+
+        </Transition>
         {this.state.modalIsOpen ? <Modal show={this.state.modalIsOpen} closed={this.closeModal} /> : null}
         {this.state.modalIsOpen ? <Backdrop show={this.state.modalIsOpen} /> : null}
         <button className="Button" onClick={this.showModal}>Open Modal</button>
         <h3>Animating Lists</h3>
         <List />
-      </div>
+      </div >
     );
   }
 }
