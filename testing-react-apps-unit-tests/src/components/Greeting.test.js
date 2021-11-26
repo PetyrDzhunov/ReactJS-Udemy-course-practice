6b28bb6374bf8b26eff942f555436783aefe705b
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2021-11-26 17:14:32 +0200

    testing our events aswell

--- testing-react-apps-unit-tests/src/components/Greeting.test.js
+++ testing-react-apps-unit-tests/src/components/Greeting.test.js
@@ -1,18 +1,47 @@
 import { render, screen } from "@testing-library/react";
 import Greeting from "./Greeting";
+import userEvent from '@testing-library/user-event';
 
+describe('Greeting component', () => {
+	test('renders "Hello World" as a text', () => {
+		//Arrange
+		render(<Greeting />);
+		//Act
+		//... nothing
+		//Assert
+		const helloWorldElement = screen.getByText('Hello World');
+		expect(helloWorldElement).toBeInTheDocument();
+	});
 
-test('renders Hello World as a text', () => {
-	//Arrange
-	render(<Greeting />);
+	test('renders good to see you if the button was NOT clicked', () => {
+		render(<Greeting />);
 
-	//Act
-	//... nothing
+		const shouldBeSeen = screen.getByText(/It's good to see you!/i);
+		expect(shouldBeSeen).toBeInTheDocument();
+	});
 
-	//Assert
-	const helloWorldElement = screen.getByText('Hello World');
+	test('renders "Changed" if the button WAS clicked', () => {
+		//Arrange
+		render(<Greeting />);
 
-	expect(helloWorldElement).toBeInTheDocument();
+		//Act
+		const buttonElement = screen.getByRole('button');
+		userEvent.click(buttonElement);
 
+		//Assert
+		const outputElement = screen.getByText(/changed/i);
+		expect(outputElement).toBeInTheDocument();
+	});
+
+	test('only one paragraph is visible when the button WAS clicked', () => {
+
+		render(<Greeting />);
+
+		const buttonElement = screen.getByRole('button');
+		userEvent.click(buttonElement);
+
+		const outputElement = screen.queryByText(/good to see you/i);
+		expect(outputElement).toBeNull();
+	});
 
 });
