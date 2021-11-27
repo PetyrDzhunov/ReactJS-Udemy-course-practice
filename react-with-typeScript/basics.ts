00a12f3804291f354dd239f115c70f77a5391f6b
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2021-11-27 13:13:14 +0200

    object type definition

--- react-with-typeScript/basics.ts
+++ react-with-typeScript/basics.ts
@@ -4,6 +4,24 @@ let age: number;
 
 age = 12;
 
+let userName: string;
+userName = "Stelko97";
+
+let isInstructor: boolean;
+
+isInstructor = true;
+
+let hobbies: string[] = ['Sports', 'Cooking', 'Travelling'];
+
+let person: { age: number, name: string };
+
+person = {
+	name: 'Max',
+	age: 32
+};
+
+
+
 
 
 //More complex types:arrays,objects
