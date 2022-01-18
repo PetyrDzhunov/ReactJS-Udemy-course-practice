abcd4e5d17d2602d7a3825719b8371c90c89f499
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2022-01-18 17:33:01 +0200

    initial commit on nextJS application

--- react-hooks/src/components/Ingredients/IngredientForm.js
+++ react-hooks/src/components/Ingredients/IngredientForm.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Card from '../UI/Card';
 import './IngredientForm.css';
 
 const IngredientForm = React.memo(props => {
+  const [input, setInput] = useState({ title: '', amount: '' });
+
+
   const submitHandler = event => {
     event.preventDefault();
     // ...
+  };
+
+  const changeTitleHandler = (e) => {
+    setInput({ ...input, title: e.target.value });
+  };
+
+  const changeNumberHandler = (e) => {
+    setInput({ ...input, amount: e.target.value });
   };
 
   return (
@@ -15,11 +26,11 @@ const IngredientForm = React.memo(props => {
         <form onSubmit={submitHandler}>
           <div className="form-control">
             <label htmlFor="title">Name</label>
-            <input type="text" id="title" />
+            <input onChange={changeTitleHandler} value={input.title} type="text" id="title" />
           </div>
           <div className="form-control">
             <label htmlFor="amount">Amount</label>
-            <input type="number" id="amount" />
+            <input onChange={changeNumberHandler} value={input.amount} type="number" id="amount" />
           </div>
           <div className="ingredient-form__actions">
             <button type="submit">Add Ingredient</button>
